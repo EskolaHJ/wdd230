542892eefb05ba42ea28d17ed2409ae06c581a22
EskolaHJ <esk23003@byui.edu>
2024-10-16 20:25:15 -0600

    Bom page done

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button')
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', () => {
+    // Check if the input is not empty
+    if (input.value.trim() !== '') {
+        //Create an li element
+        const listItem = document.createElement('li');
+
+        // Create a delete button
+        const deleteButton = document.createElement('button');
+
+        // Set the li text to the input value
+        listItem.textContent = input.value;
+
+        // Set the delete button text to ❌
+        deleteButton.textContent = '❌';
+
+        // Append the delte button to the li
+        listItem.appendChild(deleteButton);
+
+        // Append the li to the list
+        list.appendChild(listItem);
+
+        //Add an event listener to the delete button that removes the li element when clicked
+        deletButton.addEventListener('click', () => {
+            list.removeChild(listItem);
+        });
+
+        // Clear the imput field and set focus back to it
+        input.value = '';
+        input.focus();
+    } else {
+        // If input is empty, send focus back to the input field
+        input.focus();
+    }
+});
