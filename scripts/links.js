f65a7638f96b566edf597b7fcedfab566fe1bb54
EskolaHJ <esk23003@byui.edu>
2024-11-13 09:17:53 -0700

    fixed syntax error

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,7 @@ const linksURL = "https://eskolahj.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
-    const data = await Response.json();
+    const data = await response.json();
     console.log(data); // Verify the data loads correctly
 
     displayLinks(data); // Call displayLinks with the JSON data
